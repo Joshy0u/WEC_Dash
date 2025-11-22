a9564ddd80f17503f70bb57c058b93db754224a8
Joshua <joshuajhn8@gmail.com>
2025-11-22 14:36:32 -0600

    server.py changes

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,13 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
+if __name__ =='__main__':
+    app.run()
+
+print(__name__)
+
+
+
 @app.route('/')
 def hello_world():
     return "<p>Hello, World!</p>"
