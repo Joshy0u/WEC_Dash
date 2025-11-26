1405c4e1bdf2b62efa71695cc6bf0ddfac3e6448
Joshua <joshuajhn8@gmail.com>
2025-11-26 13:31:01 -0600

    update routing/README

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,25 @@
 from flask import Flask
 from flask_cors import CORS
-
+from flask import request 
+from markupsafe import escape
+# so didnt realize that with flask im actually building an API rather than using REST or GraphQL or something
 app = Flask(__name__)
 
-if __name__ =='__main__':
-    app.run()
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "World")
+    return f"Hello, {escape(name)}!"
+
+@app.route('/')
+def index(): 
+    return 'Index Page'
 
 print(__name__)
 
+if __name__ =='__main__':
+    app.run() # note have this at the end, have the routes above it
 
 
-@app.route('/')
-def hello_world():
-    return "<p>Hello, World!</p>"
+# just run via python backend/server.py
+
+
